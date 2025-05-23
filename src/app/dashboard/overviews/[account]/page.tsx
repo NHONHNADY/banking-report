883ae39d2269a304f7f3df8fcb51b3cb43a6b672
NHONHNADY <nady22548@gmail.com>
2025-05-23 19:10:14 +0700

    use param

---
+++ src/app/dashboard/overviews/[account]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useParams } from "next/navigation";
+
+export default function AccontPage() {
+  const params = useParams<{ account: string }>();
+  return (
+    <div className="flex flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <h1 className="text-2xl font-bold">
+          Acount Overview: {params.account}
+        </h1>
+        <p className="mt-4 text-muted-foreground">
+          This is the overview page of the dashboard.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/dashboard/overviews/page.tsx
+++ src/app/dashboard/overviews/page.tsx
@@ -2,11 +2,11 @@
 import { useParams } from "next/navigation";
 
 export default function OverviewPage() {
-  const params = useParams<{ param: string }>();
+  const params = useParams<{ account: string }>();
   return (
     <div className="flex flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <h1 className="text-2xl font-bold">Overview: {params.param}</h1>
+        <h1 className="text-2xl font-bold">Overview: {params.account}</h1>
         <p className="mt-4 text-muted-foreground">
           This is the overview page of the dashboard.
         </p>
